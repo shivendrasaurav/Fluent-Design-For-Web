9fe282b1b8251a7a31722a75f0a88d856db4a5f3
Shivendra Saurav <shivendrasaurav@gmail.com>
2019-07-15 16:15:17 +0530

    Added Buttons And Overlays

---
+++ fluent.js
@@ -0,0 +1,10 @@
+function frosting(){
+
+    document.querySelector(".frost_container:hover").onmousemove = e => {
+        const x = e.pageX - e.target.offsetLeft;
+        const y = e.pageY - e.target.offsetTop;
+    
+        e.target.style.setProperty("--x", `${x}px`);
+        e.target.style.setProperty("--y", `${y}px`);
+    };
+}
